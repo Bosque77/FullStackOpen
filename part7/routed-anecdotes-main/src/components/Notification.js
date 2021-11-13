88becd8a8d58f8e63bd243f06f7358edd3aa795f
forest <f.schwartz@localeventsupplies.com>
2021-11-12 19:51:19 -0800

    finished 7.3

---
+++ part7/routed-anecdotes-main/src/components/Notification.js
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+
+const Notification = ({notification}) => {
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+
+  if (notification) {
+    return (
+      <div style={style}>
+        {notification}
+      </div>
+    )
+  } else {
+    return (
+      <div >
+      </div>
+    )
+  }
+
+}
+
+export default Notification
